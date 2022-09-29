5c71e9450cec0bec6cbe300544cba7c48e787a8d
gyagoo <ish96116@gmail.com>
2022-09-29 20:45:31 +0900

    [김유정] - 배고 작업 시작

--- src/main/java/com/yoojung0318/puppyplay/PuppyPlayApplication.java
+++ src/main/java/com/yoojung0318/puppyplay/PuppyPlayApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //DB 설정 제외 문법 ( 최초 테스트시 사용)
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 
 @SpringBootApplication
-public class PuppyPlayApplication {
+public class PuppyPlayApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PuppyPlayApplication.class, args);
